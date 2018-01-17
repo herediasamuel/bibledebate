e928c4b32add874ee68e771c4b640085dff740d2
SamoxPro <samuel.heredia@smartclarity.com>
2018-01-16 23:44:22 -0300

    Welcome - First Page

--- app/src/main/java/com/example/smarclarity/bible_ebate/view/MainActivity.kt
+++ app/src/main/java/com/example/smarclarity/bible_ebate/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.smarclarity.bible_ebate
+package com.example.smarclarity.bible_ebate.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.smarclarity.bible_ebate.R
 
 class MainActivity : AppCompatActivity() {
 
